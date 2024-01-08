f15d787ad3b55ded4eff74b13539c30bee4be78b
REZZ <turbo.emamhasani@gmail.com>
2024-01-08 21:14:16 +0330

    add rank portotype + banner changer

--- bot-v1.py
+++ bot-v1.py
@@ -1,9 +1,9 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands,tasks
 import os
 from dotenv import load_dotenv
 import json
-
+import random
 # Load the .env file
 load_dotenv()
 
@@ -15,13 +15,29 @@ with open('auto_react.json','r', encoding='utf-8') as f:
 # Get the token from the .env file
 TOKEN = os.getenv('token')
 
-intents = discord.Intents.default()
+
+intents = discord.Intents.default() 
 intents.reactions = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='MR', intents=intents)
+
+# load banner images 
+banner_images = []
+for image in os.listdir('banners'):
+    if image.endswith('.png') or image.endswith('.jpg'):
+        banner_images.append(image)
+
+@tasks.loop(seconds=5) 
+async def change_banner():
+    guild = bot.get_guild(712292895515738134) 
+    if guild.premium_tier >= 2:
+        banner = random.choice(banner_images)
+        with open(f'banners/{banner}', 'rb') as f:
+            await guild.edit(banner=f.read())
 
 @bot.event
 async def on_ready():
+    change_banner.start()
     print(f'Logged in as {bot.user.name}')
 
 @bot.event
@@ -42,5 +58,6 @@ async def roleplay(ctx):
 async def play(ctx):
     # Your music playback code here
     pass
+
 
 bot.run(TOKEN)
---
+++ rank-prototype.py
@@ -0,0 +1,72 @@
+import discord
+from collections import defaultdict
+from discord.ext import commands, tasks
+import json
+from dotenv import load_dotenv
+
+
+bot = commands.Bot(command_prefix='!') 
+
+# Dict to store voice state data
+voice_states = {}
+
+# Track game time totals
+game_times = defaultdict(int) 
+
+@tasks.loop(minutes=1)  
+async def update_leaderboard():
+    current_lb = {}
+    
+    # Build leaderboard dict
+    for member_id, data in voice_states.items():
+        member = bot.get_user(member_id)
+        game = data["game"]
+        time = data["time"]
+        
+        # Update total game time 
+        game_times[game] += time
+        
+        if member.id not in current_lb:
+            current_lb[member.id] = time
+        else:
+            current_lb[member.id] += time
+            
+    # Sort leaderboard        
+    lb_sorted = sorted(current_lb.items(), 
+                       key=lambda x: x[1], reverse=True)
+
+    # Save leaderboard
+    with open("leaderboard.txt", "w") as f: 
+        f.write(json.dumps(lb_sorted)) 
+
+
+@update_leaderboard.before_loop
+async def before_print():
+    await bot.wait_until_ready()
+    
+update_leaderboard.start()  
+
+@bot.command()
+async def leaderboard(ctx):
+    with open("leaderboard.txt") as f:
+        lb = json.load(f)
+        
+    embed = discord.Embed(title="Leaderboard")
+    for i, (member_id, time) in enumerate(lb, start=1): 
+        member = bot.get_user(int(member_id))
+        embed.add_field(name=f"{i}. {member}", value=f"{time//60} hours")
+
+    await ctx.send(embed=embed)
+    
+@bot.command()   
+async def topgames(ctx):
+    sorted_games = sorted(game_times.items(), 
+                          key=lambda x: x[1], reverse=True)
+                          
+    embed = discord.Embed(title="Most Played Games")
+    for i, (game, time) in enumerate(sorted_games[:5], start=1):
+        embed.add_field(name=f"{i}. {game}", value=f"{time//60} hours") 
+    
+    await ctx.send(embed=embed)
+
+bot.run('token')
